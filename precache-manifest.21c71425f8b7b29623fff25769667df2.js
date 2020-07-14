c0c33c39d4ca5d59ab5ee7ab74bdd260ea7fd42d
davidggevorgyan <davidggevorgyan@gmail.com>
2020-07-15 02:46:33 +0400

    Updates

---
+++ precache-manifest.21c71425f8b7b29623fff25769667df2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ec39bbf36fc6a9966e48deb587c026",
+    "url": "/davidggevorgyan/todo-react/index.html"
+  },
+  {
+    "revision": "8c40f3c44ca1b83c9992",
+    "url": "/davidggevorgyan/todo-react/static/css/main.eb3ca735.chunk.css"
+  },
+  {
+    "revision": "7619d13b4a0d75aac20f",
+    "url": "/davidggevorgyan/todo-react/static/js/2.7adee998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/davidggevorgyan/todo-react/static/js/2.7adee998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c40f3c44ca1b83c9992",
+    "url": "/davidggevorgyan/todo-react/static/js/main.347a1a01.chunk.js"
+  },
+  {
+    "revision": "44502f0b3d0c4252621b",
+    "url": "/davidggevorgyan/todo-react/static/js/runtime-main.d57a56e6.js"
+  }
+]);
